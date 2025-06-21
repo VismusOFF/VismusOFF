36ea67b3e6ed840651100cb8323dcbea1cd33e14
John Doe <johndoe@example.com>
2025-06-21 17:09:00 +0300

    fix portfolio

--- VismusOFF/src/components/develoer.jsx
+++ VismusOFF/src/components/develoer.jsx
@@ -8,11 +8,20 @@ const Developer = () => {
                 <h2>Мои сайты</h2>
                 <div className='websiteText'>
                     <a href="https://zxcmusic.onrender.com/"> Курсовая 3 курс</a>
-                    <a href="https://ptacticecrud.onrender.com/main"> Первая практика</a>
+                    <a href="https://ptacticecrud.onrender.com/"> Первая практика</a>
                     <a href="https://respirator.netlify.app/"> Вторая практика</a>
                     <a href="https://crudapp-dc3d.onrender.com/">Третья практика</a>
                     <a href="https://mdkcrudapp.netlify.app/">Практическая по мобильной разработке</a>
-                    <a href="https://technodinamica.netlify.app/">Диплом?</a>
+                    <a href="https://technodinamica.netlify.app/">Дипломная работа</a>
+                </div>
+                <h2>Заказы</h2>
+                <div className='websiteText'>
+                    <a href="https://pavlovsliyposad.netlify.app/"> Курсовая 3 курс</a>
+                    <a href="https://respiratortime.netlify.app/"> Дипломная работа</a>
+                </div>
+                <h2>В разработке</h2>
+                <div className='websiteText'>
+                    <a href="https://voltageauto.netlify.app/"> Сайт для грузинской автомастерской</a>
                 </div>
             </div>
         </section>
